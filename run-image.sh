63594bf1a17131f0b3cc634e8b2cf7c1c352203d
Chris Troutner <chris.troutner@gmail.com>
2019-05-05 07:54:38 -0700

    Adding ZMQ support to the full node

--- run-image.sh
+++ run-image.sh
@@ -2,4 +2,5 @@
 docker container run --name bitcoind -d \
 -p 8332:8332 -p 8333:8333 -p 18332:18332 -p 18333:18333 \
 -v /mnt/data/bitcoin.com/blockchain-data/bch-mainnet-abc-rpi:/data \
+--rm \
 bitcoind
