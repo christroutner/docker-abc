2580b9e57a518c42d898f430bc545b2366ed2705
Chris Troutner <chris.troutner@gmail.com>
2019-05-06 17:04:57 -0700

    Modifying for testnet.

--- run-image.sh
+++ run-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker container run --name bitcoind -d \
+docker container run --name bitcoind-testnet -d \
 -p 8332:8332 -p 8333:8333 -p 18332:18332 -p 18333:18333 \
--v /mnt/data/bitcoin.com/blockchain-data/bch-mainnet-abc-rpi:/data \
+-v /mnt/data/bitcoin.com/blockchain-data/bch-testnet-abc:/data \
 --rm \
-bitcoind
+bitcoind-testnet
