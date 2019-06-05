8677e676561f2192f67262e075c9eb1db3e7728b
Chris Troutenr <chris.troutner@gmail.com>
2019-06-05 16:12:39 -0700

    Customized for testnet on toolbox HD

--- run-image.sh
+++ run-image.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker container run --name bitcoind-testnet -d \
--p 8332:8332 -p 8333:8333 -p 18332:18332 -p 18333:18333 -p 28333:28333 \
--v /home/safeuser/testnet-blockchain-data:/data \
---rm \
+-p 18332:18332 -p 18333:18333 -p 38333:38333 \
+-v /media/usb/full-nodes/testnet/abc-testnet:/data \
 bitcoind-testnet
